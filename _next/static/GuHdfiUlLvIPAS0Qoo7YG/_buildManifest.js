2944848ae59ffaea5ee3fa8f9435f4c06d9a5888
deepak <deepakmalhotra2070@gmail.com>
2020-05-11 10:31:51 +0530

    test baseURL

---
+++ _next/static/GuHdfiUlLvIPAS0Qoo7YG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002Ff069561003d3edce093eb126659ed043303fa122.6209b8fda39ad6460cce.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
